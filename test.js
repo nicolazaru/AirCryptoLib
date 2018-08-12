cdad540a8a992b3490f175896903fe780c72fb73
lakeba-solutions <devteam@lakeba.com>
2018-08-12 19:22:19 +0200

    Add files via upload

--- test.js
+++ test.js
@@ -1,16 +1,18 @@
 
 'use strict'
 
+// require module
+var AirCrypto = require ('./index.js');
 
-var aircrypto = require ('./index.js');
-
+// use the AirCrypto class inside the AirCrypto module
+var aircrypto = new AirCrypto.AirCrypto("CFF7D059-5EFC-49B2-BD18-B937261943B5","b25c9ae6f91d4c9f948b67ec00113456");
 var paymentID = "";
+
 // Quantities to get from user
 var amount = 234;
-var primaryCurrency = "AUD"; // or USD ?
-var secondaryCurrency = "XRP";
+var primaryCurrency = "AUD";
+var secondaryCurrency = "XRP"; // or "ETH", "BTC", "LTC", "BCH",  you choose
 var userEmail = "user@gmail.com";
-var MerchantRefNumber = "345345345"; // ??
 
 aircrypto.getQuote(amount, primaryCurrency, secondaryCurrency).then(function (res) {
   let quote = JSON.parse(res);
@@ -18,7 +20,7 @@ aircrypto.getQuote(amount, primaryCurrency, secondaryCurrency).then(function (re
 });
 
 
-
+// input: email, the currency in crypto (secondaryCurrency in this case), amount in AUD
 aircrypto.createPayment(userEmail, secondaryCurrency, amount).then(function (res) {
   let payment = JSON.parse(res);
   paymentID = payment.Data.PaymentID;
